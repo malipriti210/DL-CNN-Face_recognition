54f5acb0d683d5af0cb3e91f3dc3f26ba74398d0
malipriti210 <malipriti210@gmail.com>
2026-07-19 11:53:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ from PIL import Image
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 
-
-st.title("Face Mask Detection")
 if "open_camera" not in st.session_state:
     st.session_state.open_camera = False
-    
-model = load_model("mask_final.keras")
+st.title("Face Mask Detection")
 
+import os
+import requests
+
+MODEL_URL = "https://github.com/malipriti210/DL-CNN-Face_recognition/releases/download/v1.0/mask_final.keras"
+
+if not os.path.exists("mask_final.keras"):
+    response = requests.get(MODEL_URL)
+    with open("mask_final.keras", "wb") as f:
+        f.write(response.content)
+
+model = load_model("mask_final.keras")
 uploaded_file = st.file_uploader("Upload an Image ",type = ["jpg","jpeg","png"])
 
 if uploaded_file is not None:
@@ -48,16 +56,50 @@ with col2:
 if st.session_state.open_camera:
 
     camera_image = st.camera_input("Click Photo")
+
     if camera_image is not None:
-        
+
         img = Image.open(camera_image)
-        img = img.resize((128, 128))
-        img_array = image.img_to_array(img)
-        img_array = img_array / 255.0
+        img = img.resize((128,128))
+
+        img_array = image.img_to_array(img) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
 
         prediction = model.predict(img_array)
         confidence = prediction[0][0]
+
+        if confidence > 0.5:
+            st.success(f"Without Mask 😷 ({confidence:.2%})")
+        else:
+            st.success(f"With Mask ✅ ({(1-confidence):.2%})")
+
+        st.session_state.open_camera = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         if confidence > 0.5:
             st.success(f"Without Mask 😷 ({confidence:.2%})")
